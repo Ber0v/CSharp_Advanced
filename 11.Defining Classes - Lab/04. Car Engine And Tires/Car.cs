48dfb49480cc09d3653892f4616f94c97aadeb6b
Ber0v <AlexBerov123@gmail.com>
2024-01-28 19:57:15 +0200

    Defining Classes - Lab
    solving all tasks from Defining Classes - Lab

---
+++ 11.Defining Classes - Lab/04. Car Engine And Tires/Car.cs
@@ -0,0 +1,27 @@
+﻿
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private Engine engine;
+        private Tire[] tires;
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+        public Engine Engine
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+
+        public Tire[] Tires
+        {
+            get { return tires; }
+            set { tires = value; }
+        }
+    }
+}
---
+++ 11.Defining Classes - Lab/04. Car Engine And Tires/Engine.cs
@@ -0,0 +1,50 @@
+﻿namespace CarManufacturer
+{
+    public class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+
+        public double CubicCapacity
+        {
+            get { return cubicCapacity; }
+            set { cubicCapacity = value; }
+        }
+    }
+
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double Pressure
+        {
+            get { return pressure; }
+            set { pressure = value; }
+        }
+    }
+}
