a97ae72d68e840aa6cb9a9387d8d1fb3806fcc02
Ber0v <AlexBerov123@gmail.com>
2024-01-29 22:43:15 +0200

    Defining Classes - Exercise
    solving all tasks from Defining Classes - Exercise

---
+++ 12.Defining Classes - Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person peter = new Person("Peter", 20);
+            Person george = new Person() { Name = "George", Age = 18 };
+            Person jose = new Person() { Name = "Jose", Age = 43 };
+        }
+    }
+}
