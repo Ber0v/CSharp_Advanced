b9ed81be7d8409e23a0fbdb38fd7207b91cac506
Ber0v <AlexBerov123@gmail.com>
2024-01-31 21:04:55 +0200

    fix

--- 12.Defining Classes - Exercise/02. Creating Constructors/obj/Debug/net6.0/02. Creating Constructors.AssemblyInfo.cs
+++ 12.Defining Classes - Exercise/02. Creating Constructors/obj/Debug/net6.0/02. Creating Constructors.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02. Creating Constructors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97ae72d68e840aa6cb9a9387d8d1fb3806fcc02")]
 [assembly: System.Reflection.AssemblyProductAttribute("02. Creating Constructors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02. Creating Constructors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
