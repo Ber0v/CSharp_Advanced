d5205687f196debd0f203a13d4d6d1c57c682767
Ber0v <AlexBerov123@gmail.com>
2024-02-08 19:18:32 +0200

    Iterators and Comparators - Lab
    solving all tasks from Iterators and Comparators - Lab

---
+++ 15.Iterators and Comparators - Lab/01. Library/Library.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books;
+        private int index;
+
+        public LibraryIterator(List<Book> books)
+        {
+            this.books = books;
+            this.index = -1;
+        }
+
+        public Book Current => this.books[index];
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            this.index++;
+            return this.index < this.books.Count;
+        }
+
+        public void Reset()
+        {
+            this.index = -1;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
